c53453f815c272329c4a16fd0b2f6a13f270420d
chenzefan <491473594@qq.com>
2017-08-23 22:44:10 +0800

    create project by pycharm python-version 3.6.1 & django-version 1.11.3

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from requests import request
+from books.models import Publisher, Author, Book
 
-# Create your views here.
+
+def search(request):
+    error = False
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains = q)
+            return render_to_response('search_result.html', {'books': books, "query": q})
+    return render_to_response('search_form.html', {'error': error})
+
+
